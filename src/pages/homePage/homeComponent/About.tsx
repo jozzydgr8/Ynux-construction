5a251af3538d25e3c12d04c323a2f16632b49d0b
jozzydgr8 <jozzydgreat1@gmail.com>
2026-05-28 01:07:37 -0700

    added about section

--- src/pages/homePage/homeComponent/About.tsx
+++ src/pages/homePage/homeComponent/About.tsx
@@ -1,17 +1,24 @@
+import { FlatButton } from "../../../shared/FlatButton";
+import {ArrowRightOutlined} from '@ant-design/icons';
+import aboutImage from '../../../assets/whitemaninConstruction.png';
+
 export const About = ()=>{
     return(
         <section id="about">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-6">
-
-                    </div>
+                    <div className="col-md-6 d-flex mb-4">
+                        <div style={{width:'100%',
+                        
+                        height:'100%',backgroundImage:`url(${aboutImage})`, backgroundPosition:'center', backgroundSize:'cover', minHeight:'300px'}}></div>
+                        </div>
+                        
 
                     <div className="col-md-6 animate-up">
                         <div>
                             <span className='homeBadgeParent '> 
                             <span className='homeBadge'></span>
-                                Our Services
+                                about ynux
                             </span>
                         </div>
                                     
@@ -29,12 +36,26 @@ export const About = ()=>{
                         </p>
 
 
-                    <p>
-                        We have consistently delivered engineering and construction projects across multiple cities in Nigeria, combining quality, innovation,
-                         and technical expertise to provide reliable project execution from concept to completion.
-                    </p>
+                        <p>
+                            We have consistently delivered engineering and construction projects across multiple cities in Nigeria, combining quality, innovation,
+                            and technical expertise to provide reliable project execution from concept to completion.
+                        </p>
+
+                        <ul className="row">
+                            <li className="col-md-6 animate-up">International Best Practices</li>
+                            <li className="col-md-6 animate-up">Technical Expertise</li>
+                            <li className="col-md-6 animate-up">Transparent Delivery</li>
+                            <li className="col-md-6 animate-up">Sustainable Development</li>
+                        </ul>
+                        <br/>
+
+                        <FlatButton title='learn more about us ' className='btnPrimary btn btn-xl' icon={<ArrowRightOutlined/>}/>
+                    </div>
+
+
+
                 </div>
-            </div>
+
             </div>
         </section>
     )
