87234e98b115e98678f08858f654f72cc972b6d7
jozzydgr8 <jozzydgreat1@gmail.com>
2026-06-01 13:44:18 -0700

    fixed wrong number

--- src/data.tsx
+++ src/data.tsx
@@ -122,12 +122,12 @@ export const contactInfo = [
   {
     icon: <PhoneFilled style={{...contactIcon}}/>,
     label: "Call us directly",
-    value:<a href="tel:+234 808 899 1286">+234 123 454 6789</a>
+    value:<a href="tel:+234 808 899 1286">+234 808 899 1286</a>
   },
   {
     icon: <MailFilled style={{...contactIcon}}/>,
     label: "Email us anytime",
-    value: <a href="mailto:info@ynuxconstruction">info@ynuxconstruction</a>
+    value: <a href="mailto:yinuxintegratedconstruction@gmail.com">yinuxintegratedconstruction@gmail.com</a>
   },
   {
     icon: <EnvironmentFilled style={{...contactIcon}}/>,
